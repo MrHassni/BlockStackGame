96ccdddc752b9d8d03b5ae325a2201c34c9ad21e
M. Hassan Arshad <90133453+MrHassni@users.noreply.github.com>
2026-02-19 22:53:25 +0500

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "jenkins-s3-bucket-for-project"
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
