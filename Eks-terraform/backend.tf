318dea8f8961a8443165c414e2c2d8960f115a79
M. Hassan Arshad <90133453+MrHassni@users.noreply.github.com>
2026-02-19 23:05:21 +0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "eks-s3-bucket-for-project"
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
